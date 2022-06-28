6866be68d7ca4f4d25419a5c64c9a02360def251
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-28 12:55:18 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,9 +1,29 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
-
-prompt.get('input', function (err, result) {
+prompt.get("input", function (err, result) {
+  var input = result.input; // "5,3 5 6 8 9"
+  var input_data = input.split(","); // ['5', '3 5 6 8 9']
+  var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
+  var arr = input_data[1].split(" "); // Daxil edilmiş massiv. Nümunə - ['3', '5', '6', '8', '9']
+  let max = 0;
+  let min = 100;
+  let sum=0;
+    for(i=0; i<arr.length; i++){
   
-    // Write code here
-  
+    if( arr[i] > max){
+      max=arr[i];
+    }
+    }
+  for(i=0; i<arr.length; i++){
+    if( arr[i]<min){
+      min=arr[i]
+    }
+  }
+  for(i=0; i<arr.length; i++){
+    if(arr[i]!=max && arr[i]!=min){
+      sum= sum+arr[i]
+    }
+  }
+  console.log(sum)
 });
