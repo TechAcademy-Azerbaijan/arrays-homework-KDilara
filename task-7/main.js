28ec6b36a4e957097d4582d9466e8a79b0a7be5d
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-28 17:13:35 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -7,7 +7,6 @@ prompt.get("input", function (err, result) {
   var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
   var arr = input_data[1].split(" "); // Daxil edilmiş massiv. Nümunə - ['3', '5', '6', '8', '9']
   let min = 100;
-  let arr2 = [];
   for(i=0; i<arr.length; i++){
     if( arr[i]<min){
       min=arr[i]
