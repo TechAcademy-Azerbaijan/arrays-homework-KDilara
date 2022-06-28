bdd3eccae5a9393c1864c2d42e2eac9d8162ec66
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-28 11:04:59 +0400

    Update example5.js

--- instructions/example5.js
+++ instructions/example5.js
@@ -1,20 +1,18 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
-
-prompt.get('input', function (err, result) {
-    /* 
-    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
-    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
-    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
-    */
-    var input = result.input; // "5,2 4 7 12 9"
-    var input_data = input.split(','); // ['5', '2 4 7 12 9']
-    var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
-    var massiv = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
-    var sum = 0;
-    for(i=0;i<n;i++){
-        sum+=parseInt(massiv[i])
+prompt.get("input", function (err, result) {
+  var input = result.input; // "5,2 4 7 12 9"
+  var input_data = input.split(","); // ['5', '2 4 7 12 9']
+  var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
+  var arr = input_data[1].split(" "); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
+  let max = 0;
+  for (i = 1; i <= n; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
-    console.log(sum); // 34
+  }
+
+  console.log(max);
+  console.log(arr.indexOf(max) + 1);
 });
