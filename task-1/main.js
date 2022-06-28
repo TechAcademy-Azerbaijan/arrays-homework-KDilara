2592bdac63c4d25c0c5fa2211a44ae120587bbe6
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-28 19:11:03 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -6,5 +6,11 @@ prompt.get("input", function (err, result) {
   var input_data = input.split(","); // ['5', '3 5 6 8 9']
   var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
   var arr = input_data[1].split(" "); // Daxil edilmiş massiv. Nümunə - ['3', '5', '6', '8', '9']
-  console.log(arr.length-1)
+  const arr2[];
+  for(i=0; i<arr.length; i++){
+    if( !arr2.includes(arr[i])){
+      arr2.push(arr[i])
+    }
+    }
+  console.log(arr2.length)
 });
